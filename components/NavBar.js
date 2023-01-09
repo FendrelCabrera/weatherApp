3f42f511ac2bd6748c377b9300e28fe80022a239
FendrelCabrera <shalin36shayne@gmail.com>
2023-01-09 12:49:10 +0530

    modified navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,9 +5,9 @@ import icon from '../public/w-icon-25.png'
 const Navbar = () => {
     return (
         <nav className="flex justify-between items-center text-3xl bg-cyan-600 text-white">
-            <Link className="" href="/">WeatherAlerts</Link>
+            <Link className="pl-2" href="/">WeatherAlerts</Link>
             <Image src={icon} alt="" width={50} height={50} />
-            <Link className="" href="/subscribe">Subscribe</Link>
+            <Link className="pr-2" href="/subscribe">Subscribe</Link>
         </nav>
     )
 }
